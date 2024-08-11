2da995187d9b242ebc91136818b5d726d96b876c
Niel McLaren <d@nielmclaren.com>
2024-08-11 11:52:45 -0700

    Make enemies alternate between moving and resting.

--- mob.py
+++ mob.py
@@ -1,9 +1,37 @@
+import pygame
+from mob_state import MobState
 
 class Mob:
     def __init__(self, pos):
         self.pos = pos
+        self.state = MobState.RESTING
+        self.state_change_time = pygame.time.get_ticks()
 
     def step(self, player_pos, dt):
-       direction = (player_pos - self.pos).normalize()
-       self.pos += direction * 200 * dt
-       
+        match self.state:
+            case MobState.RESTING:
+                return self.__step_resting(player_pos, dt)
+            case MobState.MOVING:
+                return self.__step_moving(player_pos, dt) 
+    
+    def __step_resting(self, player_pos, dt):
+        if (pygame.time.get_ticks() - self.state_change_time > 500):
+            self.__change_state_moving(player_pos, dt)
+    
+    def __step_moving(self, player_pos, dt):
+        if (pygame.time.get_ticks() - self.state_change_time > 250):
+            self.__change_state_resting(player_pos, dt)
+
+        direction = (self.target_pos - self.pos).normalize()
+        self.pos += direction * 400 * dt
+    
+    def __change_state_moving(self, player_pos, dt):
+        self.state = MobState.MOVING
+        self.state_change_time = pygame.time.get_ticks()
+
+        self.target_pos = player_pos.copy()
+
+    def __change_state_resting(self, player_pos, dt):
+        self.state = MobState.RESTING
+        self.state_change_time = pygame.time.get_ticks()
+        
---
+++ mob_state.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class MobState(Enum):
+    RESTING = 0
+    MOVING = 1
